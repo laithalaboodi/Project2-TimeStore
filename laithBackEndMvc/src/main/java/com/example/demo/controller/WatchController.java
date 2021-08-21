94e7f80f6c36dc58fee329db88e8c2c14763a6df
jrusso8199 <jrusso8199@gmail.com>
2021-08-20 22:58:35 -0400

    fixed watch in service

--- laithBackEndMvc/src/main/java/com/example/demo/controller/WatchController.java
+++ laithBackEndMvc/src/main/java/com/example/demo/controller/WatchController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Watch;
-import com.example.demo.service.WatchService;
+import com.example.demo.services.WatchService;
 
 @RestController
 @RequestMapping(value="/watches")
