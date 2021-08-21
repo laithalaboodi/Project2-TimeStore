3fafa426ec2a8cf6c0b961ae66f9e98485812ca8
jrusso8199 <jrusso8199@pop-os.localdomain>
2021-08-20 22:04:24 -0400

    Rebase new repo

---
+++ laithBackEndMvc/src/main/java/com/example/demo/controller/WatchController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Watch;
+import com.example.demo.service.WatchService;
+
+@RestController
+@RequestMapping(value="/watches")
+@CrossOrigin(origins="*")
+public class WatchController {
+	private final WatchService watchService;
+	
+	@Autowired
+	public WatchController(WatchService watchService) {
+		this.watchService = watchService;
+	}
+	
+	@GetMapping
+	public List<Watch> getWatches(){
+		return watchService.getWatchs();
+	}
+
+}
